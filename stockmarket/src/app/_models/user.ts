015cf27cc1eb5cd8e39d78018bbc1fc0ddfaefd8
Tamas Vajgely <vajgi90@gmail.com>
2020-01-24 21:23:13 +0100

    changed structure

---
+++ stockmarket/src/app/_models/user.ts
@@ -0,0 +1,8 @@
+﻿export class User {
+    "access_token": string;
+    "token_type": string;
+    "refresh_token": string;
+    "expires_in": number;
+    "scope": string;
+    "jti": string;
+}
---
+++ stockmarket/src/app/_services/authentication.service.ts
@@ -0,0 +1,45 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    login(username: string, password: string) {
+        const body = new HttpParams()
+            .set('grant_type', 'password')
+            .set('username', username)
+            .set('password', password);
+        return this.http.post<any>(`http://localhost:8080/oauth/token`, body.toString(),
+         {
+            headers: new HttpHeaders()
+            .set('Authorization', 'Basic ' + btoa('fooClientIdPassword:password'))
+              .set('Content-Type', 'application/x-www-form-urlencoded')
+          })
+            .pipe(map(user => {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('currentUser', JSON.stringify(user));
+                this.currentUserSubject.next(user);
+                return user;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+}
